7fcd1259cc554dda6de8a9ce80f4ee43cc5a69cf
Curran <curran.kelleher@gmail.com>
2014-10-22 20:56:16 -0400

    Added CSS theme

--- startLocalServer.sh
+++ startLocalServer.sh
@@ -1,4 +1,4 @@
 # Starts a local HTTP server for development use.
 # Curran Kelleher October 2014
 # See http://www.pythonforbeginners.com/modules-in-python/how-to-use-simplehttpserver/
-python -m SimpleHTTPServer 8080
+python -m SimpleHTTPServer 8080 &
